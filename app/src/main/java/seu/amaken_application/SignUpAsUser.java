2c61a8500a0015854fd78a517aa94428831e699c
afaf <shoog-alroo7_@hotmail.com>
2017-02-04 14:12:42 +0300

    Activity Sign Up as User UC-3 from Prototype( Sign Up as User UC-3  )

---
+++ app/src/main/java/seu/amaken_application/SignUpAsUser.java
@@ -0,0 +1,13 @@
+package seu.amaken_application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpAsUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_as_user);
+    }
+}
